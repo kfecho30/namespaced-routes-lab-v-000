60f3f8d47495e18213f9eb5c31be076cee0e10bb
Kelsey Fecho <kfecho30@gmail.com>
2018-05-09 17:52:31 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,7 +1,6 @@
 class Admin::PreferencesController < ApplicationController
 
   def index
-    @preferences = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_songs: true, allow_create_artists: true)
   end
 
 end
