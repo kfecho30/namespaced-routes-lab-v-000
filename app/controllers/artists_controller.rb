575ff48edebdc471202c1014a0c334d2fa18a51d
Kelsey Fecho <kfecho30@gmail.com>
2018-05-09 17:47:27 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,7 +11,7 @@ class ArtistsController < ApplicationController
     if @preferences.allow_create_artists
       @artist = Artist.new
     else
-      redirect_to artists_path
+      redirect_to artists_path, alert: "You're not allowed to create new artists."
     end
   end
 
